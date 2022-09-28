7d04493c79272f58f003021c32024d062ee75739
samuel cordova <cordovas1337@gmail.com>
2022-09-28 13:00:21 -0500

    added the code to remove the previous active class from a panel

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ const panels = document.querySelectorAll('.panel');
 
 
 // you can loop through a node list like you can with an array.
-panels.forEach((panel) =>  {
+panels.forEach(panel =>  {
 panel.addEventListener('click', () => {
     removeActiveClasses()
     panel.classList.add('active');
-}
-)})
+})
+})
 
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active');
+    })
+}
