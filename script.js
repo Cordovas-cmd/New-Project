86a13cdbcc679b8979e6de76b8d7fb7d27085ff9
samuel cordova <cordovas1337@gmail.com>
2022-09-28 22:11:55 -0500

    trying to test out different ways of adding and removing classes to style the cards appropriately

--- script.js
+++ script.js
@@ -3,14 +3,46 @@ const panels = document.querySelectorAll('.panel');
 
 // you can loop through a node list like you can with an array.
 panels.forEach(panel =>  {
+
+    if(!panel.classList.contains('.active')){
+        panel.classList.add('inactive');
+        panel.classList.remove('active');
+    } else {
+    }
 panel.addEventListener('click', () => {
     removeActiveClasses()
-    panel.classList.add('active');
+    
+    if(panel.classList.contains('.active') || !panel.classList.contains('inactive')) {
+        removeActiveClasses()
+        panel.classList.remove('inactive');
+    }
+    else {
+        panel.classList.add('active')
+        panel.classList.remove('inactive')
+    }
 })
 })
 
 function removeActiveClasses() {
     panels.forEach(panel => {
-        panel.classList.remove('active');
+        if(panel.classList.contains('active')) {
+        panel.classList.remove('active') 
+        panels.classList.add('inactive')
+        // panel.classList.add('inactive')
+    } 
+    else{
+    // (!panel.classList.contains('inactive'))
+    
+        panel.classList.add('inactive')}
+    //     else {
+    //         return;
+    //     }
+
+        // else {
+        //     panel.classList.add('active');
+        // }
+        // if(panel.matches('.active')) {
+        //     panel.classList.remove('inactive');
+        // }
     })
 }
